d578a9dee305fb2a23712e23055b0a5a2dd92973
jonard-debug <jonardbethuel@gmail.com>
2026-02-18 10:18:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
+from telethon.sync import TelegramClient
 import asyncio
-from telethon import TelegramClient
-from telethon.sessions import StringSession
 
-API_ID = '24395315'
+API_ID = 24395315
 API_HASH = '9ea9861f2c047bb3ebc0de86675c0238'
+PHONE = '++2348130641310' # Put your number here!
 
-# 1. PUT YOUR PHONE NUMBER HERE (e.g., '+2348012345678')
-PHONE = '+2348130641310' 
+async def main():
+    client = TelegramClient('session', API_ID, API_HASH)
+    await client.connect()
+    if not await client.is_user_authorized():
+        await client.send_code_request(PHONE)
+        print("🏛️ CODE SENT! CHECK YOUR TELEGRAM APP NOW!")
 
-# 2. RUN THIS ONCE, IT WILL CRASH, BUT CHECK YOUR TELEGRAM FOR THE CODE
-# 3. THEN PUT THE CODE TELEGRAM SENT YOU HERE
-CODE = 'NONE' 
-
-async def generate():
-    async with TelegramClient(StringSession(), API_ID, API_HASH) as client:
-        if CODE == 'NONE':
-            await client.send_code_request(PHONE)
-            print("🏛️ CHECK YOUR TELEGRAM APP FOR THE LOGIN CODE!")
-        else:
-            await client.sign_in(PHONE, CODE)
-            print("🏆 YOUR SESSION STRING IS BELOW: 🏆")
-            print(client.session.save())
-            print("🏆 COPY THE LONG TEXT ABOVE! 🏆")
-
-asyncio.run(generate())
+asyncio.run(main())
